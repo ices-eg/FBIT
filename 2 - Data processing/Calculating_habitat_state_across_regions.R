13b7dbdedee3ce0252c532df5a1854e431dd18bb
Pieter Daniël van Denderen <pdvd@win.dtu.dk>
2020-06-25 19:34:31 +0200

    update

---
+++ 2 - Data processing/Calculating_habitat_state_across_regions.R
@@ -0,0 +1,18 @@
+
+### load sensitivity layer and fishing to calculate impact for an ecoregion
+
+# load the grid for the region
+  load(paste(pathdir,"1 - Input env",paste(EcoReg,"region_grid_sensitivity.RData",sep="_"),sep="/")) 
+
+# load the Fisheries for the region
+  load(paste(pathdir_nogit,paste(EcoReg,"fisheries.RData",sep="_"),sep="/")) 
+  load(paste(pathdir_nogit,paste(EcoReg,"fisheries_per_metier.RData",sep="_"),sep="/")) 
+
+# calculate state and impact for all gears 
+  # and three different metier groups TBBall, OTCRU and OTREST
+  setwd(paste(pathdir,"Utilities",sep="/"))
+  source("Impact_continuous_longevity.R")
+  source("Habitatstatefishing.R") ## takes a few minutes
+  
+  setwd(pathdir_nogit)
+  save(State_reg,file=paste(EcoReg,"state.RData",sep="_"))
