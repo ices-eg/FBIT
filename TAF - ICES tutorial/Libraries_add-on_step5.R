489034eb2c7bfbfa91ab2a2e4388576729c4e86f
DvanDenderen <danielvden@hotmail.com>
2025-10-04 13:00:33 +0200

    few edits

---
+++ TAF - ICES tutorial/Libraries_add-on_step5.R
@@ -0,0 +1,8 @@
+
+# Libraries to make parallel run and create maps
+library(foreach)
+library(doParallel)
+library(iterators)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(latex2exp)
--- TAF - ICES tutorial/Libraries_needed.R
+++ TAF - ICES tutorial/Libraries_needed.R
@@ -2,12 +2,8 @@
 # libraries
 library(sf)
 library(ggplot2)
-library(rnaturalearth)
 library(dplyr)
 library(Matrix)
 library(lme4)
 library(logitnorm)
-library(foreach)
-library(doParallel)
-library(iterators)
 library(stats)
